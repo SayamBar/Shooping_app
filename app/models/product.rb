f12fccda3d8c2ee121eb8710b2592d5dfa55dae6
SayamBar <barsayam@gmail.com>
2023-02-16 15:03:38 +0530

    users added,authenticate using bcrypt gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     has_many :orders, :through => :product_orders
+    has_one_attached :file, dependent: :destroy
 
     validates :title, presence: true, length: { minimum: 1}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "products#index"
-  # get "/products", to:"products#index", as: 'products'
-  # post "/products", to:"products#create"
-  # get "/products/new", to:"products#new", as:'new_product'
-  # get "/products/:id", to:"products#edit",as: 'edit_product'
-  # get "/products/:id", to:"products#show", as: 'product'
-  # patch "/products/:id", to:"products#update"
-  # put "/products/:id", to:"products#update"
+  get "/products", to:"products#index", as: 'products'
+  post "/products", to:"products#create"
+  get "/products/new", to:"products#new", as:'new_product'
+  get "/products/:id/edit", to:"products#edit",as: 'edit_product'
+  get "/products/:id", to:"products#show", as: 'product'
+  patch "/products/:id", to:"products#update"
+  put "/products/:id", to:"products#update"
 
-  resources :products
+  get "/signup", to:"users#new"
+  post "/users", to:"users#create"
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+  # resources :products
 end
